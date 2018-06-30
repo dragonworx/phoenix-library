11de160804e8f2fc7477e689b303960507e839af
Ali Chamas <achamas@atlassian.com>
2018-06-30 16:28:25 +1000

    stop writing log

--- server/log.js
+++ server/log.js
@@ -1,16 +1,8 @@
 const clc = require('cli-color');
-const fs = require('fs');
-const path = require('path');
-
-const logFile = path.resolve(__dirname, '../log.txt');
 
 module.exports = function log(obj, color = 'blue') {
   const output = typeof obj === 'object'
     ? JSON.stringify(obj, null, 4)
     : '' + obj;
-    if (!fs.existsSync(logFile)) {
-      fs.writeFileSync(logFile, '');
-    }
-    fs.appendFileSync(logFile, output + '\n');
   console.log(clc[color].bold(output));
 };
